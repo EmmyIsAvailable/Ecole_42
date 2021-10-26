54d6be42d9ac47228c8921b48f83ecf7e4ed0ea6
EmmyIsAvailable <emmy.ruellan@orange.fr>
2021-10-26 18:24:44 +0200

    oui

--- libft/main.c
+++ libft/main.c
@@ -8,5 +8,6 @@ int	main()
 	int	pt_excl = 33;
 	int	tab = 9;
 	int	delete = 127;
-	printf("a : %d\nA : %d\nzero : %d\npt_excl : %d\ntab : %d\ndelete : %d\n", ft_isalpha(a), ft_isalpha(A), ft_isalpha(zero), ft_isalpha(pt_excl), ft_isalpha(tab), ft_isalpha(delete));
+	/*printf("a : %d\nA : %d\nzero : %d\npt_excl : %d\ntab : %d\ndelete : %d\n", ft_isalpha(a), ft_isalpha(A), ft_isalpha(zero), ft_isalpha(pt_excl), ft_isalpha(tab), ft_isalpha(delete));*/
+	printf("%ld\n", ft_strlen("coucou"));
 }
