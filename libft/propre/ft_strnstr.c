f4a3b6d0bfc99f2d354b44a5737cbb2cbf00497a
Emmeline RUELLAN <eruellan@e3r11p19.clusters.42paris.fr>
2021-11-24 13:43:29 +0100

    libft propre

---
+++ libft/propre/ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strnstr.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eruellan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 11:15:51 by eruellan          #+#    #+#             */
+/*   Updated: 2021/11/23 16:18:55 by eruellan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+	char	*cast_s1;
+	char	*cast_s2;
+
+	i = 0;
+	cast_s1 = (char *)s1;
+	cast_s2 = (char *)s2;
+	if (!cast_s2)
+		return (cast_s1);
+	while (cast_s1[i] && i < n)
+	{
+		j = 0;
+		while (cast_s1[i + j] && cast_s2[j]
+			&& cast_s1[i + j] == cast_s2[j] && i + j < n)
+			j++;
+		if (cast_s2[j] == '\0')
+			return (&cast_s1[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ libft/propre/ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eruellan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/23 11:29:40 by eruellan          #+#    #+#             */
+/*   Updated: 2021/11/24 11:25:04 by eruellan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	char	*cast_s;
+
+	i = 0;
+	cast_s = (char *)s;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(cast_s))
+		return ("");
+	if (start + len > ft_strlen(cast_s))
+		len = ft_strlen(cast_s) - start;
+	sub = (char *)malloc(sizeof(char) * len + 1);
+	if (!sub)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = cast_s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
