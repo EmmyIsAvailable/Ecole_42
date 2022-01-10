1df0332b2be1cbef7625942c840c6ee7cded5282
EmmyIsAvailable <91387559+EmmyIsAvailable@users.noreply.github.com>
2022-01-10 10:16:59 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eruellan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 10:08:40 by eruellan          #+#    #+#             */
+/*   Updated: 2022/01/10 10:08:57 by eruellan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	**ft_free_tab(char **data)
+{
+	int	i;
+
+	i = -1;
+	while (data[++i])
+	{
+		free(data[i]);
+		data[i] = NULL;
+	}
+	return (NULL);
+}
+
+void	ft_error(char *str)
+{
+	perror(str);
+	exit(EXIT_FAILURE);
+}
+
+t_data	arg_to_data(int ac, char **av, char **envp)
+{
+	t_data	tmp;
+
+	tmp.ac = ac;
+	tmp.av = av;
+	tmp.envp = envp;
+	tmp.infile = 0;
+	tmp.outfile = 0;
+	return (tmp);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_data	data;
+	int		i;
+
+	i = 1;
+	data = arg_to_data(ac, av, envp);
+	if (ac < 5)
+	{
+		ft_putstr_fd("Format : ./pipex file1 cmd1 ... cmdn file2\n", 2);
+		return (-1);
+	}
+	else
+	{
+		data.infile = open(av[1], O_RDONLY);
+		data.outfile = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 777);
+		if (data.infile < 0 || data.outfile < 0)
+			return (-1);
+		while (++i < data.ac - 2)
+			pipex(&data);
+	}
+	return (0);
+}
---
+++ parsing.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eruellan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 10:10:51 by eruellan          #+#    #+#             */
+/*   Updated: 2022/01/10 10:10:53 by eruellan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char	*search_envp(t_data *data)
+{
+	char	*path;
+	int		i;
+
+	i = -1;
+	path = NULL;
+	while (data->envp[++i])
+	{
+		path = ft_strnstr(data->envp[i], "PATH=", ft_strlen(data->envp[i]));
+		if (path)
+		{
+			path = ft_substr(path, 5, ft_strlen(path));
+			if (!path)
+				ft_error("Error");
+			return (path);
+		}
+	}
+	return (NULL);
+}
+
+char	**get_envp(t_data *data)
+{
+	char	*env_path;
+	char	**paths;
+
+	env_path = search_envp(data);
+	paths = ft_split(env_path, ':');
+	free (env_path);
+	if (!paths)
+		ft_error("split failed or error occured getting envp");
+	return (paths);
+}
+
+char	*get_binary(char *cmd, char **env_path)
+{
+	char	*tmp;
+	char	*cmd_path;
+	int		i;
+
+	i = -1;
+	tmp = NULL;
+	cmd_path = NULL;
+	while (env_path[++i])
+	{
+		tmp = ft_strjoin(env_path[i], "/");
+		if (!tmp)
+			ft_error("tmp join failed");
+		cmd_path = ft_strjoin(tmp, cmd);
+		if (!cmd_path)
+			ft_error("cmd_path join failed");
+		free(tmp);
+		if (access(cmd_path, F_OK) == 0)
+			return (cmd_path);
+		free(cmd_path);
+	}
+	ft_error("error occured getting binary file in parsing");
+	return (NULL);
+}
